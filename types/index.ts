0058c0a03173b0c97b13945bf46e37e7e306163a
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2024-11-28 18:09:54 +0530

    make filters optional

--- types/index.ts
+++ types/index.ts
@@ -31,11 +31,11 @@ export interface CarProps {
 }
 
 export interface FilterProps{
-  manufacturer: string;
-  year: number;
-  fuel: string;
-  limit: number;
-  model: string;
+  manufacturer?: string;
+  year?: number;
+  fuel?: string;
+  limit?: number;
+  model?: string;
 }
 
 export interface HomeProps{
